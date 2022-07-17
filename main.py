18d647f5823e3e97892bcbd3db2966729d9bb61e
zroga95 <zroga95@gmail.com>
2022-07-17 15:48:41 -0400

    procedurally deriving character names

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import glob
-from parse_corpus import parse_script_lines
+from parse_corpus import parse_dialogue
 from comparing_strings import getClosestSentiment
 
 if __name__ == '__main__':
 
-	character_lines = parse_script_lines('Brazil_script.html', 'TUTTLE')
+	character_lines = parse_dialogue('Brazil_script.html')
 	submitted_str = input('Type a string to send. Leave empty for default string')
 
 	if submitted_str != '':
@@ -13,4 +13,6 @@ if __name__ == '__main__':
 	else:
 		tester = "I'm creating some simple chatbots using natural langugage processing and sentiment."
 	print('You sent: ' + tester)
-	print('Tuttle replies: ' + getClosestSentiment(character_lines, tester))
+
+	selected_character = 'TUTTLE'
+	print(selected_character + ' replies: ' + getClosestSentiment(character_lines[selected_character], tester))
--- parse_corpus.py
+++ parse_corpus.py
@@ -1,13 +1,35 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 import re
 
-def parse_script_lines(path, character_name):
-    counter = 0
-    character_lines = []
+def parse_script_characters(path):
+    counter = {}
+    characters = {}
     with open(path, 'r') as f:
         lines =  f.readlines()
         for i in range(0, len(lines)):
-            if character_name.upper() +'\n' in lines[i].upper():
+            if lines[i].strip() in counter:
+                counter[lines[i].strip()] = counter[lines[i].strip()] + 1
+            else:
+                counter[lines[i].strip()] = 1
+
+    for i in counter:
+        if((len(i) > 0) & (counter[i] > 1) & (i.upper() == i) 
+                & (i != '\n') & ('INT.' not in i)
+                & ('EXT.' not in i) & ('CUT TO' not in i)):
+            characters[i] = counter[i]
+
+    return characters
+
+def parse_script_lines(path, character_name):
+    counter = 0
+    character_lines = []
+    if '\n' not in character_name:
+        character_name += '\n'
+
+    with open(path, 'r') as f:
+        lines =  f.readlines()
+        for i in range(0, len(lines)):
+            if character_name.upper() in lines[i].upper():
                 temp = ''
                 for x in range(1,20):
                     if lines[x + i].strip() != '':
@@ -15,13 +37,28 @@ def parse_script_lines(path, character_name):
                     else:
                         break
                 #print(str(temp))
-                if (len(str(temp)) > 10) & (len(str(temp)) < 500):
-                    character_lines.append(str(temp))
+                joined_str = str(temp)
+                if (len(joined_str) > 10) & (len(joined_str) < 500):
+                    #remove anything in parenthesis
+                    character_lines.append(re.sub("\(.*?\)","",joined_str))
                 #print("\n', '\n'" in temp)
                 #print([lines[x + i] for x in range(1,20)])
-                counter = counter +1
+                counter = counter + 1
             #breaking when non-specific issues
             if counter > 10000:
                 break
     return character_lines
     
+def parse_dialogue(path):
+
+    repeat_characters = parse_script_characters(path)
+
+    dialogue_dict = {}
+
+    for i in repeat_characters:
+        lines = parse_script_lines(path, i)
+        if len(lines) > 0:
+            dialogue_dict[i.split('\n')[0]] = lines
+
+    return dialogue_dict
+
